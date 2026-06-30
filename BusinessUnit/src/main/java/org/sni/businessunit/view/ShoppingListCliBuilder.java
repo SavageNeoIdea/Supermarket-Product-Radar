b794f19db23af2d9c838ee45d64f0fecbfbb791c
Pablo Martín Perdomo <pablo.martin118@alu.ulpgc.es>
2026-06-30 21:17:35 +0100

    Fixed a problem where calcultions did not work correctly

--- BusinessUnit/src/main/java/org/sni/businessunit/view/ShoppingListCliBuilder.java
+++ BusinessUnit/src/main/java/org/sni/businessunit/view/ShoppingListCliBuilder.java
@@ -1,4 +1,5 @@
 package org.sni.businessunit.view;
+
 import org.sni.businessunit.model.DefinitiveOptimizedItem;
 import org.sni.businessunit.model.Product;
 import java.util.List;
@@ -34,32 +35,53 @@ public class ShoppingListCliBuilder {
                 .append(SEPARATOR).append(NL);
 
         for (DefinitiveOptimizedItem item : optimizedItems) {
-
             Product jointProd = item.selectedJoint();
+            double requiredStandardQuantity = 0.0;
+
             if (jointProd != null) {
-                appendProductLine(sbJoint, jointProd);
+                appendProductLine(sbJoint, jointProd, 1.0, jointProd.getPrice());
                 jointTotal += jointProd.getPrice();
+                requiredStandardQuantity = getStandardQuantity(jointProd);
             } else {
                 sbJoint.append(String.format("- [NO DISP.] %s: Producto no disponible para optimizar%s", item.userInput(), NL));
             }
 
             Product mercadonaProd = item.selectedMercadona();
             if (mercadonaProd != null) {
-                appendProductLine(sbMercadona, mercadonaProd);
-                mercadonaTotal += mercadonaProd.getPrice();
+                if (jointProd != null) {
+                    double mercadonaStdQty = getStandardQuantity(mercadonaProd);
+                    double ratio = requiredStandardQuantity / mercadonaStdQty;
+                    double equivalentPrice = mercadonaProd.getPrice() * ratio;
+
+                    appendProductLine(sbMercadona, mercadonaProd, ratio, equivalentPrice);
+                    mercadonaTotal += equivalentPrice;
+                } else {
+                    appendProductLine(sbMercadona, mercadonaProd, 1.0, mercadonaProd.getPrice());
+                    mercadonaTotal += mercadonaProd.getPrice();
+                }
             } else {
                 sbMercadona.append(String.format("- [MERCADONA] %s: ❌ No disponible en esta tienda%s", item.userInput(), NL));
             }
 
             Product hiperdinoProd = item.selectedHiperdino();
             if (hiperdinoProd != null) {
-                appendProductLine(sbHiperdino, hiperdinoProd);
-                hiperdinoTotal += hiperdinoProd.getPrice();
+                if (jointProd != null) {
+                    double hiperdinoStdQty = getStandardQuantity(hiperdinoProd);
+                    double ratio = requiredStandardQuantity / hiperdinoStdQty;
+                    double equivalentPrice = hiperdinoProd.getPrice() * ratio;
+
+                    appendProductLine(sbHiperdino, hiperdinoProd, ratio, equivalentPrice);
+                    hiperdinoTotal += equivalentPrice;
+                } else {
+                    appendProductLine(sbHiperdino, hiperdinoProd, 1.0, hiperdinoProd.getPrice());
+                    hiperdinoTotal += hiperdinoProd.getPrice();
+                }
             } else {
                 sbHiperdino.append(String.format("- [HIPERDINO] %s: ❌ No disponible en esta tienda%s", item.userInput(), NL));
             }
         }
 
+        // --- FOOTERS ---
         sbJoint.append("--------------------------------------------------------------------").append(NL)
                 .append(String.format("💰 COSTE TOTAL OPTIMIZADO (Yendo a ambos): %.2f€%s", jointTotal, NL))
                 .append(SEPARATOR).append(NL).append(NL);
@@ -81,15 +103,18 @@ public class ShoppingListCliBuilder {
         appendAnalysis(sbAnalysis, "HIPERDINO", hiperdinoTotal, jointTotal);
         sbAnalysis.append(SEPARATOR).append(NL);
 
-        return String.valueOf(sbJoint) +
-                sbMercadona +
-                sbHiperdino +
-                sbAnalysis;
+        return sbJoint.toString() + sbMercadona.toString() + sbHiperdino.toString() + sbAnalysis.toString();
     }
 
-    private void appendProductLine(StringBuilder sb, Product p) {
-        sb.append(String.format("- [%-10s] %-30s: %.2f€ | Cantidad: %.2f (%s)%s",
-                p.getSource().toUpperCase(), p.getName(), p.getPrice(), p.getQuantity(), p.getMeasure(), NL));
+    private void appendProductLine(StringBuilder sb, Product p, double ratio, double finalPrice) {
+        double displayedQuantity = p.getQuantity() * p.getPackageQuantity() * ratio;
+
+        sb.append(String.format("- [%-10s] %-30.30s: %.2f€ | Cantidad: %.2f (%s)%s",
+                p.getSource().toUpperCase(), p.getName(), finalPrice, displayedQuantity, p.getMeasure(), NL));
+    }
+
+    private double getStandardQuantity(Product p) {
+        return p.getQuantity() * p.getPackageQuantity() * p.getMeasure().getFactorToSI();
     }
 
     private void appendAnalysis(StringBuilder sb, String store, double storeTotal, double jointTotal) {
